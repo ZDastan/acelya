819031b995a025724b86ca68ef1eee8d724ed00e
zehra <zehradastan@hotmail.com>
2023-06-22 11:21:15 +0100

    sec commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const projects = [
+    {
+        title: "Coffee Shop",
+        subtitle: "MERN Stack",
+        description:
+          "The aim of this project was to create a e-commerce website for a Coffee Shop using MERN, GraphQL and Stripe.",
+        image: "./images/project-2.png",
+        link: "https://warm-harbor-00106.herokuapp.com/",
+      },
+    {
+        title: "Book Search",
+        subtitle: "MERN Stack",
+        description:
+          "The Book search engine uses the MERN stack and with a React front end, MongoDB database, and Node js & Express js server.",
+        image: "./images/project-1.png",
+        link: "https://jaza.herokuapp.com/",
+      },
+      
+      {
+        title: "Marvel Charcter Finder",
+        subtitle:"OMDb API",
+        description:
+          "The search use Api is Marvel Api where we get Marvel character information and comics and other api is OMDb Api where we get movie information.",
+        image: "./images/project-3.png",
+        link: "https://emlarko.github.io/marvel-character-finder/",
+      },
+      {
+        title: "The Tech Blog",
+        subtitle:"mySQL",
+        description:
+          "This is CMS-style blog site that where developers can publish their blog posts and comment on other developers post as well.",
+        image: "./images/project-4.png",
+        link: "https://zehtech-app.herokuapp.com/",
+      },
+];
+
+export const skills = ["JavaScript", "HTML", "CSS", "GraphQL", "React", "PHP"];
