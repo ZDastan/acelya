47a245fb4fd576dcae0ae945bb704a135aab4b62
zehra <zehradastan@hotmail.com>
2023-07-03 14:04:12 +0100

    emailjs set

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,22 @@
-import React from "react";
-export default function Contact() {
-    const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [message, setMessage] = React.useState("");
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
+import React, {useRef} from "react";
+import emailjs from '@emailjs/browser';
 
-  function handleSubmit(e) {
+export default function Contact() {
+    const form = useRef();
+
+  const sendEmail = (e) => {
     e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
-    })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
-  }
+
+    emailjs.sendForm('service_p93x35m', 'template_68w1i8j', form.current, '805I3Wjv0rZT9RZeQ')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
+
+  
     return (<div>
 <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap min-h-screen">
@@ -60,10 +56,9 @@ export default function Contact() {
             </div>
           </div>
         </div>
-        <form
-          netlify
+        <form ref={form}
           name="contact"
-          onSubmit={handleSubmit}
+          onSubmit={sendEmail}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Contact Me
@@ -79,9 +74,9 @@ export default function Contact() {
             <input
               type="text"
               id="name"
-              name="name"
+              name="user_name" required
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setName(e.target.value)}
+            
 
             />
           </div>
@@ -92,9 +87,9 @@ export default function Contact() {
             <input
               type="email"
               id="email"
-              name="email"
+              name="user_email" required
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
+              
             />
           </div>
           <div className="relative mb-4">
@@ -104,7 +99,7 @@ export default function Contact() {
             <div className="mt-2.5">
               <input
                 type="text"
-                name="company"
+                name="user_company" required
                 id="company"
                 autoComplete="organization"
                 className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
@@ -119,9 +114,9 @@ export default function Contact() {
             </label>
             <textarea
               id="message"
-              name="message"
+              name="message" required
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setMessage(e.target.value)}
+              
             />
           </div>
           <button
